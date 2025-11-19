17661772c525c7c3638f32198001ceffcff221a9
taka230 <satsuki8396@gmail.com>
2025-11-19 17:34:16 +0900

    Update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -4,18 +4,18 @@ var router = express.Router();
 // レスポンスのデータ（ノート全件）
 const responseObjectDataAll = {
  textObject1 : {
- id: 1,
- title: 'ノート１のタイトルです',
- subTitle: 'ノート１のサブタイトルです',
- bodyText: 'ノート１の本文です'
+  id: 1,
+  title: 'ノート１のタイトルです',
+  subTitle: 'ノート１のサブタイトルです',
+  bodyText: 'ノート１の本文です'
  },
  textObject2 : {
- id: 2,
- title: 'ノート２のタイトルです',
- subTitle: 'ノート２のサブタイトルです',
- bodyText: 'ノート２の本文です'
+  id: 2,
+  title: 'ノート２のタイトルです',
+  subTitle: 'ノート２のサブタイトルです',
+  bodyText: 'ノート２の本文です'
  },
- };
+};
  
 /**
  * メモを全件取得するAPI
@@ -24,9 +24,9 @@ const responseObjectDataAll = {
  * @returns {string} data.title - タイトル
 * @returns {string} data.text - 内容
 */
- router.get('/', function (req, res, next) {
+router.get('/', function (req, res, next) {
  // 全件取得して返す
-res.json(responseObjectDataAll);
- })
+ res.json(responseObjectDataAll);
+})
 
 module.exports = router;
