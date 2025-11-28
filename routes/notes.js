247b384ab5f32ed271bedea93a7b3afeef2fc919
taka230 <satsuki8396@gmail.com>
2025-11-28 20:39:18 +0900

    Update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,32 +1,23 @@
 var express = require('express');
 var router = express.Router();
  
-// レスポンスのデータ（ノート全件）
-const responseObjectDataAll = {
- textObject1 : {
-  id: 1,
-  title: 'ノート１のタイトルです',
-  subTitle: 'ノート１のサブタイトルです',
-  bodyText: 'ノート１の本文です'
- },
- textObject2 : {
-  id: 2,
-  title: 'ノート２のタイトルです',
-  subTitle: 'ノート２のサブタイトルです',
-  bodyText: 'ノート２の本文です'
- },
-};
- 
-/**
- * メモを全件取得するAPI
- * @returns {Object[]} data
- * @returns {number} data.id - ID
- * @returns {string} data.title - タイトル
-* @returns {string} data.text - 内容
-*/
-router.get('/', function (req, res, next) {
- // 全件取得して返す
- res.json(responseObjectDataAll);
+
+
+//接続情報を設定
+const{MongoClient} = require("mongodb");
+const uri = "mongodb+srv://taka230:trigger8396@cluster0.5t0sigv.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(uri);
+
+router.get('/',async (requestAnimationFrame, res) => {
+	//データベース、コレクションを指定
+	const database = client.db('notes');
+	const notes = database.collection('notes');
+
+	//idが1のドキュメントを取得
+	const query = { id:2 };
+	const note = await notes.findOne(query);
+
+	res.json(note.title);
 })
 
 module.exports = router;
